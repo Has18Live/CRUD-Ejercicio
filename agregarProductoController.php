a109278e572bf65905a8ba87a0a2750a0b22fb6b
Has <hlozada5@misena.edu.co>
2024-03-20 18:10:49 -0500

    Rama agregar

---
+++ agregarProductoController.php
@@ -0,0 +1,16 @@
+<?php
+include ('conexion.php');
+$conexion = new Conexion();
+$conn = $conexion->conectar();
+
+
+$nombre = $_POST['nombre'];
+$descripcion = $_POST['descripcion'];
+
+$sql = "INSERT INTO productos (nombre, descripcion) VALUES ('$nombre','$descripcion')";
+$query = mysqli_query($conn, $sql);
+
+if ($query) {
+    Header ("Location: index.php");
+}
+?>
